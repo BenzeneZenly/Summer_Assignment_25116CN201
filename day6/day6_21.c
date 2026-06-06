26f6f4ba5501fc6d43fe8432080209fa3f9836b5
KHUSHI TIWARY <khushitiwary@KHUSHIs-MacBook-Air.local>
2026-06-06 19:47:09 +0530

    Added day 6

---
+++ day6/day6_21.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+int main() {
+    int n, binary[32], i = 0, j;
+
+    printf("Enter decimal number: ");
+    scanf("%d", &n);
+
+    while (n > 0) {
+        binary[i++] = n % 2;
+        n /= 2;
+    }
+
+    for (j = i - 1; j >= 0; j--)
+        printf("%d", binary[j]);
+
+    return 0;
+}
